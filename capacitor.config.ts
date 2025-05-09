0531104cc4abba631cb627053ec62d0da26c543b
Olli Luukas <olli@luukas.fi>
2025-05-09 18:12:12 +0300

    Updated android application version and packages for the app itself

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'fi.luukas.sra',
   appName: 'SRA säännöt',
-  webDir: 'dist/sra-rules',
-  bundledWebRuntime: false,
+  webDir: 'dist/sra-rules/browser',  
   backgroundColor: '#eee',
   ios: {
     "preferredContentMode": "mobile"
