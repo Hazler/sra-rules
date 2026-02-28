815471f470d5467cc663ad063bc6091949aedd7e
Olli Luukas <olli@luukas.fi>
2026-02-28 22:01:12 +0200

    Updated capacitor packages and installed plugin for android edge-to-edge support

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,11 @@ const config: CapacitorConfig = {
   backgroundColor: '#eee',
   ios: {
     "preferredContentMode": "mobile"
+  },
+  plugins: {
+    SystemBars: {
+      insetsHandling: "disable"
+    }
   }
 };
 
